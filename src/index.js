ee36b4165723aceb325cd0b96ff54513c1288556
karthik2214 <71806384+karthik2214@users.noreply.github.com>
2021-03-02 03:33:46 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+<React.Fragment>  <h1> My favorite Anime </h1>
+  <p> hii guys I am Zero </p>
+<ol>
+  <li> One piece</li>
+  <li> Naruto</li>
+  <li> Attack on titan</li>
+  <li> Death note</li>
+  <li> hunter x hunter</li>
+</ol>
+  </React.Fragment> ,
+ document.getElementById('root'));
+ 
